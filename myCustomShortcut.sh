e1971d8d6d6b478bb93177a62277ff984ad9beb4
jake <palatable7@naver.com>
2023-03-26 03:42:27 +0900

    Add sorting

--- myCustomShortcut.sh
+++ myCustomShortcut.sh
@@ -1,4 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-# input
-echo "test"
+arr=()
+
+while read line || [[ -n "$line" ]]; do
+    arr+=("$line")
+done < /dev/stdin
+
+# Sort the array
+# sorted_arr=($(printf '%s\n' "${arr[@]}" | sort))
+
+arr_length=${#arr[@]}
+
+# Sort the array manually
+for (( i=0; i<${arr_length}-1; i++ ))
+do
+    for (( j=$i+1; j<${arr_length}; j++ ))
+    do
+        if [[ "${arr[$i]}" > "${arr[$j]}" ]]; then
+            temp=${arr[$i]}
+            arr[$i]=${arr[$j]}
+            arr[$j]=$temp
+        fi
+    done
+done
+
+for item in "${arr[@]}"
+do
+    echo "$item"
+    # printf '%s ' "${sorted_line[@]}"
+done
